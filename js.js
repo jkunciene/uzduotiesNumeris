722ddd47e9725e39bb00db3dff19baf316a0d5fd
Mokytojas <Mokytojas@KITM.local>
2020-09-29 11:25:40 +0300

    pradzia

---
+++ js.js
@@ -0,0 +1,16 @@
+// 1 sushi
+// 2 modernist
+// 3 allrounder
+// 4 imagenatyve
+// 5 lifestyle
+// 6 axit
+// 7 paul lapkin
+// 8 uniquetech
+// 9 pink shoes
+// 10 platform debesys
+// 11 the band
+// 12 new season looks
+// 13
+
+const button= document.getElementById('button')
+button.addEventListener('click', rask)
