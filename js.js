27b635f20accaf9a44e0e3addf9f58796907822a
j.kunciene@gmail.com <48964713+jkunciene@users.noreply.github.com>
2020-09-29 19:38:57 +0300

    persimaniau, suku cikla, kiek klaseje vaiku

--- js.js
+++ js.js
@@ -14,12 +14,25 @@
 console.log("labas");
 const mygtukas= document.getElementById('mygtukas');
 mygtukas.addEventListener('click', gaukSkaiciu);
+const vietaPrintinimui = document.getElementById('kur')
 
 function gaukSkaiciu() {
     console.log('funkcijos vidus');
-    var skaicius =  Math.floor(Math.random() * 10) + 1;
-    const vieta = document.getElementById('taskNr');
-    vieta.textContent = skaicius;
+    const lentele = document.getElementById('slepti');
+    lentele.style.display = "block";
+
+    const reiksme= parseInt(document.getElementById('kiekis').value)
+    console.log(reiksme);
+   for(let i=0; i<reiksme; i++){
+        const eilute = document.createElement('tr');
+        vietaPrintinimui.appendChild(eilute);
+        console.log('labas');
+        const stulpelis1 = document.createElement('td');
+        let skaicius =  Math.floor(Math.random() * 10) + 1;
+        stulpelis1.textContent = skaicius;
+        eilute.appendChild(stulpelis1);
+
+    }
 
 };
 
